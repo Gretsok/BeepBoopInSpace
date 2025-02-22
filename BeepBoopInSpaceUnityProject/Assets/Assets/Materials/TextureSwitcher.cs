b3886f6915c613f67013b1106628452d0c9a1876
agrimose <agriyou@gmail.com>
2025-02-22 19:10:59 +0100

    shadows et loop anims

--- BeepBoopInSpaceUnityProject/Assets/Assets/Materials/TextureSwitcher.cs
+++ BeepBoopInSpaceUnityProject/Assets/Assets/Materials/TextureSwitcher.cs
@@ -9,30 +9,25 @@ using static UnityEngine.GraphicsBuffer;
 
 [ExecuteInEditMode]
 public class TextureSwitcher : MonoBehaviour
-{
-
+{ 
 
     [Range(0, 9)]
     public int Slider;
-    public GameObject[] allChildren;
-
-    void Awake()
-    {
-        allChildren = new GameObject[transform.childCount];
-
-
-
-    }
 
     void OnValidate()
     {
 
-        for (int i = 0; i < allChildren.Length; i++)
+        for(int i = 0; i < transform.childCount; i++)
         {
-            allChildren[i] = transform.GetChild(i).gameObject;
-            transform.GetChild(i).gameObject.SetActive(false);
-            transform.GetChild(Slider).gameObject.SetActive(true);
+            var child = transform.GetChild(i);
+            if (i == Slider)
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
         }
-
     }
 }
