4bf605fe4bbedad85dca6dda1445336bc4261b1d
Fergal MECHIN <mechin.fergal@gmail.com>
2025-02-23 14:19:22 +0100

    Tutorial

---
+++ BeepBoopInSpaceUnityProject/Assets/Game/Gameplay/Flows/Tutorial/TutorialState.cs
@@ -0,0 +1,33 @@
+using Game.Gameplay.FlowMachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Gameplay.Flows.Tutorial
+{
+    public class TutorialState : AFlowState
+    {
+        [SerializeField]
+        private Button m_nextButton;
+
+        [SerializeField]
+        private AFlowState m_nextState;
+
+        protected override void HandleEnter()
+        {
+            base.HandleEnter();
+            
+            m_nextButton.onClick.AddListener(HandleNextButtonClicked);
+        }
+
+        protected override void HandleLeave()
+        {
+            base.HandleLeave();
+            m_nextButton.onClick.RemoveListener(HandleNextButtonClicked);
+        }
+
+        private void HandleNextButtonClicked()
+        {
+            RequestState(m_nextState);
+        }
+    }
+}
