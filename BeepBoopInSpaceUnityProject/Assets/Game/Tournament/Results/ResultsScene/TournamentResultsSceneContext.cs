8c00c2ace3c8e2557e6b59a55e138283f746a977
Fergal MECHIN <mechin.fergal@gmail.com>
2026-04-19 15:30:29 +0200

    gamepad in tournament results

--- BeepBoopInSpaceUnityProject/Assets/Game/Tournament/Results/ResultsScene/TournamentResultsSceneContext.cs
+++ BeepBoopInSpaceUnityProject/Assets/Game/Tournament/Results/ResultsScene/TournamentResultsSceneContext.cs
@@ -1,5 +1,8 @@
+using System.Collections;
 using Game.ArchitectureTools.Manager;
+using Game.Global;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Tournament.Results.ResultsScene
 {
@@ -7,5 +10,31 @@ namespace Game.Tournament.Results.ResultsScene
     {
         [field: SerializeField]
         public TournamentResultsCanvas ResultsCanvas { get; private set; }
+
+        protected override IEnumerator Initialize()
+        {
+            yield return base.Initialize();
+            
+            var globalContext = GlobalContext.Instance;
+            globalContext.NavigationAuthorityManager.SetInputRegistrationsCallbacks(RegisterMainPlayerActions, UnregisterMainPlayerAction);
+        }
+        
+        private void RegisterMainPlayerActions(InputActionAsset actionAsset)
+        {
+            actionAsset.FindActionMap("UI").FindAction("Submit").started += HandleSubmitStartedByMainPlayer;
+        }
+
+        private void UnregisterMainPlayerAction(InputActionAsset actionAsset)
+        {
+            actionAsset.FindActionMap("UI").FindAction("Submit").started -= HandleSubmitStartedByMainPlayer;
+        }
+        
+        private void HandleSubmitStartedByMainPlayer(InputAction.CallbackContext obj)
+        {
+            ResultsCanvas.ManualClickButton();
+            
+            var globalContext = GlobalContext.Instance;
+            globalContext.NavigationAuthorityManager.UnsetInputRegistrationsCallbacks(RegisterMainPlayerActions, UnregisterMainPlayerAction);
+        }
     }
 }
